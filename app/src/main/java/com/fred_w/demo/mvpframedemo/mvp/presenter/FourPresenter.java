ef8f59c2343251f06701a30aa00e7c59084d563b
wjl <wjl7123093@163.com>
2017-12-25 19:00:55 +0800

    自动生成 Presenter

---
+++ app/src/main/java/com/fred_w/demo/mvpframedemo/mvp/presenter/FourPresenter.java
@@ -0,0 +1,44 @@
+package com.fred_w.demo.mvpframedemo.mvp.presenter;
+
+import android.app.Application;
+
+import com.jess.arms.integration.AppManager;
+import com.jess.arms.di.scope.ActivityScope;
+import com.jess.arms.mvp.BasePresenter;
+import com.jess.arms.http.imageloader.ImageLoader;
+
+import me.jessyan.rxerrorhandler.core.RxErrorHandler;
+
+import javax.inject.Inject;
+
+import com.fred_w.demo.mvpframedemo.mvp.contract.FourContract;
+
+
+@ActivityScope
+public class FourPresenter extends BasePresenter<FourContract.Model, FourContract.View> {
+    private RxErrorHandler mErrorHandler;
+    private Application mApplication;
+    private ImageLoader mImageLoader;
+    private AppManager mAppManager;
+
+    @Inject
+    public FourPresenter(FourContract.Model model, FourContract.View rootView
+            , RxErrorHandler handler, Application application
+            , ImageLoader imageLoader, AppManager appManager) {
+        super(model, rootView);
+        this.mErrorHandler = handler;
+        this.mApplication = application;
+        this.mImageLoader = imageLoader;
+        this.mAppManager = appManager;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.mErrorHandler = null;
+        this.mAppManager = null;
+        this.mImageLoader = null;
+        this.mApplication = null;
+    }
+
+}
